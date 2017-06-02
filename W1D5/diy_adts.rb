2118dc6d7848509e34dadd46e80e29cdeaf9c3c6
Riyan Christy <ryan.christy@msn.com>
2017-06-01 20:11:42 -0700

    DIY ADTs make Stack, Queue, and Map classes

---
+++ W1D5/diy_adts.rb
@@ -0,0 +1,85 @@
+class Stack
+  attr_reader :stack
+
+    def initialize
+      @stack = []
+      # create ivar to store stack here!
+    end
+
+    def add(el)
+      # adds an element to the stack
+      @stack << el
+    end
+
+    def remove
+      # removes one element from the stack
+      @stack.pop
+    end
+
+    def show
+      # return a copy of the stack
+      @stack
+    end
+
+end
+
+#FIFO
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    enqueue << el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue
+  end
+
+end
+
+
+
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    if !(@map.flatten.include?(key))
+      @map << [key, value]
+    else
+      idx = nil
+      @map.each_index do |index|
+        idx = index if @map[index][0] == key
+      end
+      @map[index][1] = value
+    end
+  end
+
+  def lookup(key)
+    @map.each_index { |index| return index if @map[index][0] == key }
+
+    nil
+  end
+
+  def remove(key)
+    idx = nil
+    @map.each_index{ |index| idx = index if @map[index][0] == key }
+
+    @map.delete_at(idx)
+  end
+
+  def show
+    @map
+  end
+
+end
